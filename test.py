1075db164ef79544ba6fe0449ab996692a344833
donkrazy <donkrazy12@gmail.com>
2017-10-14 18:29:27 +0900

    Crawl only first seed: 628.97
    urllib 씨빠알

---
+++ test.py
@@ -0,0 +1,102 @@
+import urllib.request
+import urllib.error
+import urllib.parse
+import json
+import requests
+
+
+def get_token():
+    token_login = 'Ujme8WK325GqrHRzNBE3Bpj7MHk83-gNRDdp8A3VrmM'
+    url = 'http://api.welcome.kakao.com/token/{}'.format(token_login)
+    # get new token
+    try:
+        response = urllib.request.urlopen(url)
+        with open('token', 'w') as f:
+            token_submit = response.read().decode('utf-8')
+            f.write(token_submit)
+    # 403 error: read token from file
+    # TODO: get toekn from body
+    except urllib.error.HTTPError:
+        with open('token') as f:
+            token_submit = f.read()
+    return token_submit
+
+
+def get_seed(token):
+    url = 'http://api.welcome.kakao.com/seed'
+    request = urllib.request.Request(url)
+    request.add_header("X-Auth-Token", token)
+    response = urllib.request.urlopen(request)
+    seed_list = response.read().decode('utf-8').strip().split('\n')
+    return seed_list
+
+
+def get_document(seed, token):
+    url = 'http://api.welcome.kakao.com{}'.format(seed)
+    request = urllib.request.Request(url)
+    request.add_header("X-Auth-Token", token)
+    response = urllib.request.urlopen(request)
+    document = json.loads(response.read().decode('utf-8'))
+    return document
+
+
+def manage_image(images, token):
+    image_dict = {}
+    # TODO: duplicate?
+    for img in images:
+        if img['type'] == 'add':
+            image_dict[img['id']] = 'add'
+        else:
+            image_dict[img['id']] = 'del'
+
+    to_save = []
+    to_delete = []
+    for img_id in image_dict:
+        if image_dict[img_id] == 'add':
+            to_save.append(img_id)
+        else:
+            to_delete.append(img_id)
+
+    num_post = manage_features(to_save, token, 'POST')
+    num_delete = manage_features(to_delete, token, 'DELETE')
+    return (num_post, num_delete)
+
+
+def manage_features(images, token, method):
+    # slice images if 50^ images
+    if len(images) > 50:
+        num_feature = manage_features(images[:50], token, method)
+        num_feature += manage_features(images[50:], token, method)
+        return num_feature
+
+    # get features
+    url = 'http://api.welcome.kakao.com/image/feature?id={}'.format(','.join(images))
+    request = urllib.request.Request(url)
+    request.add_header("X-Auth-Token", token)
+    response = urllib.request.urlopen(request)
+    features = json.loads(response.read().decode('utf-8'))['features']
+
+    # post or delete features
+    url = 'http://api.welcome.kakao.com/image/feature'
+    headers = {"X-Auth-Token": token}
+    if method == 'POST':
+        # r = requests.post('http://httpbin.org/post', data=json.dumps({'data': features}), headers=headers)
+        requests.post(url, data=json.dumps({'data': features}), headers=headers)
+    elif method == 'DELETE':
+        # r = requests.delete('http://httpbin.org/delete', data=json.dumps({'data': features}), headers=headers)
+        requests.delete(url, data=json.dumps({'data': features}), headers=headers)
+
+    # print(r.text)
+
+    return len(features)
+
+
+if __name__ == '__main__':
+    token = get_token()
+    seed_list = get_seed(token)
+    for seed in seed_list:
+        document = get_document(seed, token)
+        images = document['images']
+        result = manage_image(images, token)
+        # next_url = document['next_url']
+        print('got document from {}, {} images, {}/{}(post/delete) features'.format(seed, len(images), result[0], result[1]))
